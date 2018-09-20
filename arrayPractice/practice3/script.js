e9f30d30db41d530a536dcfbf962f682cde4fb46
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-09-20 22:56:45 +0800

    finish practice3

--- arrayPractice/practice3/script.js
+++ arrayPractice/practice3/script.js
@@ -3,3 +3,27 @@
 //  find two different numbers in the array that, when
 //  added together, give the target number. For example:
 //  answer([1,2,3], 4)should return [1,3]
+
+const testArray = [1,2,3];
+
+
+
+const answer = (array, number) => {
+  let result = [];
+
+  const filterFunction = (value, index, array) => {
+    return value === (number - array[i]);
+  }
+
+  for (var i = 0; i < array.length; i++){
+    let tempArray = array.filter(filterFunction);
+    if (tempArray.length !== 0){
+      tempArray.unshift(array[i]);
+      result.push(tempArray);
+    }
+  }
+  
+  return result;
+}
+
+console.log(answer(testArray, 5));
