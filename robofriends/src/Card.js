b5e602f0a224c03ea627b02efb07a90f2eab8a7c
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-09-25 00:42:40 +0800

    add robots.js to the robofriends and use properties import to the card.js by using destructuring

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const card = () => {
+const card = (props) => {
+  const {name, email, id} = props;
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src='https://robohash.org/test?200x200'/>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h2>Jane doe</h2>
-        <p>janedoe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import Card from './Card';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
+import { robots } from './robots';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].amail} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].amail} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].amail} />
+  </div>
+, document.getElementById('root'));
 registerServiceWorker();
