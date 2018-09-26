d5e8ea80c97a0f73da5c3d021eecc0c99832272c
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-09-26 18:08:08 +0800

    add CardList into the robofriends

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import 'tachyons';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
-import { robots } from './robots';
 
 ReactDOM.render(
   <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].amail} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].amail} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].amail} />
+    <CardList />
   </div>
 , document.getElementById('root'));
 registerServiceWorker();
