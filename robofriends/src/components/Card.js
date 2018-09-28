5d14c07b1fa4f810b31a4363189e136d1293fc0b
ChinCheChang <36709019+ChinCheChang@users.noreply.github.com>
2018-09-28 18:04:24 +0800

    little change'

--- robofriends/src/components/Card.js
+++ robofriends/src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const card = ({name, email, id}) => {
   return (
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=300x300`} />
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
